b8990744c68c431fa3ada0ef2ecac706b4a9dc27
dgel28 <daniel.gelfond@gmail.com>
2023-06-08 14:04:53 +0300

    added file

--- first_task.py
+++ first_task.py
@@ -1,12 +1,18 @@
 import pandas
 import pandas as pd
-#"""
+
+
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Activation, Dense
 
-from ff import FF
+
 from matplotlib import pyplot as plt
+
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
 
 import numpy as np
 from utils import split_train_test
@@ -18,6 +24,7 @@ def preprocess_data(X: pd.DataFrame, y: pd.Series):
     X['check_in'] = pd.to_datetime(df['checkin_date']).dt.dayofyear
     X['check_out'] = pd.to_datetime(df['checkout_date']).dt.dayofyear
     #X['hotel_county_code_one_hot'] = pd.Categorical(X['hotel_country_code']).cat.codes
+    X['h_customer_one_hot'] = pd.factorize(X['h_customer_id'])[0]
     X['hotel_county_code_one_hot'] = pd.factorize(X['hotel_country_code'])[0]
     X['accommodation_type_name_one_hot'] = pd.factorize(X['accommadation_type_name'])[0]
     X['charge_option_one_hot'] = pd.factorize(X['charge_option'])[0]
@@ -26,12 +33,20 @@ def preprocess_data(X: pd.DataFrame, y: pd.Series):
     X['payment_type_one_hot'] = pd.factorize(X['original_payment_type'])[0]
     X['cancellation_policy_one_hot'] = pd.factorize(X['cancellation_policy_code'])[0]
     X['is_first_booking'] = pd.factorize(X['is_first_booking'])[0]
-
+    """
+    X['original_selling_amount'] = X['original_selling_amount']/np.max(X['original_selling_amount'])
+    X['hotel_city_code'] = X['hotel_city_code']/np.max(X['hotel_city_code'])
+    X['booking_day'] = X['booking_day']/np.max(X['booking_day'])
+    X['check_in'] = X['check_in'] / np.max(X['check_in'])
+    X['check_out'] = X['check_out']/ np.max(X['check_out'])
+    X['hotel_star_rating'] = X['hotel_star_rating']
+    """
     ls = {'request_nonesmoke':0.5, 'request_latecheckin':0.5, 'request_highfloor':0.5, 'request_largebed':0.5, 'request_twinbeds':0.5, 'request_airport':0.5, 'request_earlycheckin':0.5}
     X.fillna(ls, inplace=True)
 
     X.drop(columns=["hotel_country_code", "accommadation_type_name", "charge_option", "guest_nationality_country_name", "language", "original_payment_type", "cancellation_policy_code", "booking_datetime", "checkin_date", "checkout_date",  "hotel_brand_code", "hotel_chain_code", "original_payment_method","h_booking_id", "hotel_id", "hotel_area_code", "hotel_live_date", "h_customer_id", "customer_nationality", "origin_country_code", "original_payment_currency", "is_user_logged_in"], inplace=True)
-    X.dropna(inplace=True)
+    X = (X - X.min()) / (X.max() - X.min())
+
 
     """
     y[y.notnull() == False] = -1
@@ -47,10 +62,10 @@ def preprocess_data(X: pd.DataFrame, y: pd.Series):
 
 def generate_model():
     model = Sequential()
-    model.add(tf.keras.layers.Dense(26, activation='relu'))
+    model.add(tf.keras.layers.Dense(27, activation='relu'))
     model.add(tf.keras.layers.Dense(10, activation='relu'))
     model.add(tf.keras.layers.Dense(10, activation='relu'))
-    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+    model.add(tf.keras.layers.Dense(2, activation='sigmoid'))
     model.compile(
     loss=tf.keras.losses.binary_crossentropy,
     optimizer=tf.keras.optimizers.Adam(lr=0.03),
@@ -62,10 +77,15 @@ def generate_model():
 
     return model
 
-layers_sizes = [26, 10, 1] # flexible, but should be [784,...,10]
-epochs = 10
-eta = 0.1
-batch_size = 20
+
+def encode(y: np.array):
+    y_t = np.zeros(shape=(len(y), 2))
+    y_t[:, 0] = y
+    y_t[:, 1] = 1 - y
+    return y_t
+
+def decode(y: np.array):
+    return (y[:, 0] > y[:, 1])*1
 
 
 if __name__ == "__main__":
@@ -77,21 +97,15 @@ if __name__ == "__main__":
     X_tr, y_tr = preprocess_data(X_tr, y_tr)
     X_tst, y_tst = preprocess_data(X_tst, y_tst)
 
+    y_tr = encode(y_tr)
+
     model = generate_model()
-    model.fit(X_tr, y_tr, epochs=100)#, verbose=True)  # , steps_per_epoch=50)
+    model.fit(X_tr, y_tr, epochs=100)#, verbose=True, steps_per_epoch=200)
 
     y_pred = model.predict(X_tst)
-    y_pred = np.where(y_pred<0.5, 0, 1)
-    print(np.sum(np.abs(y_pred.T - y_tst))/len(y_tst))
+    y_pred = decode(y_pred)
+    #y_pred = np.where(y_pred<0.5, 0, 1)
+    print(np.sum(np.abs(y_pred - y_tst))/len(y_tst))
+    np.savetxt('predict_1.csv', np.array([y_pred]).T, delimiter = ',')
+    np.savetxt('answer_1.csv', np.array([y_tst]).T, delimiter = ',')
 
-    """
-    net = FF(layers_sizes)
-    steps, test_acc = net.sgd(X_tr.T, y_tr.T, epochs, eta, batch_size, X_tst.T, y_tst.T)
-
-    ## plotting learning curve and visualizing some examples from test set
-
-    plt.plot(steps, test_acc, 'r')
-    plt.xlabel('steps')
-    plt.ylabel('acc')
-    plt.show()
-    """
